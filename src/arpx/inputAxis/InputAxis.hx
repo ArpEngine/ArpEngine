52cec661b230b4e1e48744dff7c759eb71a741a1
kaikoga <info@mail.kaikoga.net>
2019-06-19 22:21:54 +0900

    remove level logic from InputAxis

--- src/arpx/inputAxis/InputAxis.hx
+++ src/arpx/inputAxis/InputAxis.hx
@@ -12,9 +12,7 @@ class InputAxis implements ITickableChild<Input> implements IArpObject {
 	private var nextValue(default, default):Float = 0;
 
 	private var state(default, null):Bool = false;
-	private var level(default, null):Float = 0;
 	private var stateDuration(default, null):Float = 0;
-	private var levelDuration(default, null):Float = 0;
 	private var threshold(default, default):Float = 0.6;
 
 	public var isUp(get, null):Bool;
@@ -24,13 +22,13 @@ class InputAxis implements ITickableChild<Input> implements IArpObject {
 	inline private function get_isDown():Bool return this.state;
 
 	public var isTrigger(get, null):Bool;
-	inline private function get_isTrigger():Bool return this.levelDuration == 0;
+	inline private function get_isTrigger():Bool return this.stateDuration == 0;
 
 	public var isTriggerUp(get, null):Bool;
-	inline private function get_isTriggerUp():Bool return !this.state && this.stateDuration == 0;
+	inline private function get_isTriggerUp():Bool return this.isTrigger && !this.state;
 
 	public var isTriggerDown(get, null):Bool;
-	inline private function get_isTriggerDown():Bool return this.state && this.stateDuration == 0;
+	inline private function get_isTriggerDown():Bool return this.isTrigger && this.state;
 
 	public function new() return;
 
@@ -41,16 +39,6 @@ class InputAxis implements ITickableChild<Input> implements IArpObject {
 			this.state = newState;
 		} else {
 			this.stateDuration += timeslice;
-		}
-
-		var newLevel:Int = Math.floor(this.nextValue / this.threshold);
-		var newLevel:Int = Math.floor(this.nextValue / this.threshold);
-		if (newLevel < 0) newLevel++;
-		if (this.level != newLevel) {
-			this.levelDuration = 0;
-			this.level = newLevel;
-		} else {
-			this.levelDuration += timeslice;
 		}
 
 		this.value = this.nextValue;
