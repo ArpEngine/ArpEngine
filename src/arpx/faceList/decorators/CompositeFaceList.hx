6fdc570b0b0d560ba6e4880cd72fa29fa46ee8fc
kaikoga <info@mail.kaikoga.net>
2020-01-04 00:29:44 +0900

    materialize CompositeFaceList

--- src/arpx/faceList/decorators/CompositeFaceList.hx
+++ src/arpx/faceList/decorators/CompositeFaceList.hx
@@ -1,43 +1,18 @@
 package arpx.faceList.decorators;
 
 import arp.ds.IList;
-import arp.iterators.CompositeIterator;
 
 @:arpType("faceList", "composite")
-class CompositeFaceList extends FaceList {
+class CompositeFaceList extends ArrayFaceList {
 
 	@:arpField(true) @:arpBarrier public var faceLists:IList<FaceList>;
 	public function new() super();
 
-	override private function get_length():Int return Lambda.fold(faceLists, (i, m) -> i.length + m, 0);
-
-	override public function indexOf(face:String):Int {
-		var pos:Int = 0;
+	override private function populate():Void {
 		for (faceList in faceLists) {
-			var index:Int = faceList.indexOf(face);
-			if (index >= 0) return index + pos;
-			pos += faceList.length;
+			for (faceSpan in faceList) {
+				this.add(faceSpan.face, faceSpan.dirs);
+			}
 		}
-		return -1;
 	}
-
-	override public function faceSpan(face:String):Null<FaceSpan> {
-		for (faceList in faceLists) {
-			var faceSpan:Null<FaceSpan> = faceList.faceSpan(face);
-			if (faceSpan != null) return faceSpan;
-		}
-		return null;
-	}
-
-	override public function faceSpanAt(index:Int):Null<FaceSpan> {
-		var pos:Int = 0;
-		for (faceList in faceLists) {
-			var len:Int = faceList.length;
-			if (pos + len > index) return faceList.faceSpanAt(index - pos);
-			pos += len;
-		}
-		return null;
-	}
-
-	override public function iterator():Iterator<FaceSpan> return new CompositeIterator(this.faceLists);
 }
