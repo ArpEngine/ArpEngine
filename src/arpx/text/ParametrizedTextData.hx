1e97331d9f46be5c2041260f20bd2b365a101419
kaikoga <info@mail.kaikoga.net>
2020-02-21 08:29:42 +0900

    refactor: format TextData in customFormatter

--- src/arpx/text/ParametrizedTextData.hx
+++ src/arpx/text/ParametrizedTextData.hx
@@ -13,26 +13,16 @@ class ParametrizedTextData extends TextData {
 	public function new() super();
 
 	@:arpHeatUp private function heatUp():Bool {
-		if (this.value == null) return true;
 		if (this.impl != null) return true;
-		this.impl = new FormatText(this.value);
+		var value = if (this.value == null) "null" else this.value;
+		var customFormatter = (param:Any) -> if (Std.is(param, TextData)) (param:TextData).publish() else null;
+		this.impl = new FormatText(value, customFormatter);
 		return true;
 	}
 
 	override public function publish(params:ArpParams = null):String {
 		if (this.impl == null) this.heatUp();
 
-		return if (params == null) {
-			this.value;
-		} else {
-			this.impl.publish(key -> {
-				var value:Dynamic = params.get(key);
-				return if (Std.is(value, TextData)) {
-					cast(value, TextData).publish();
-				} else {
-					Std.string(value);
-				}
-			});
-		}
+		return if (params == null) this.value else this.impl.publish((name:String) -> params.get(name));
 	}
 }
