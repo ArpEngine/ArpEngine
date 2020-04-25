130419e00b8edcb7c48c785b29c1cc21c24a3abc
kaikoga <info@mail.kaikoga.net>
2020-04-26 04:14:30 +0900

    also absolute curve

--- src/arpx/motionFrame/CurveMotionFrame.hx
+++ src/arpx/motionFrame/CurveMotionFrame.hx
@@ -13,37 +13,62 @@ class CurveMotionFrame extends MotionFrame {
 	@:arpField public var z:ArpCurve;
 	@:arpField public var r:ArpCurve;
 	@:arpField public var s:ArpCurve;
+	@:arpField public var absolute:Bool = false;
 
 	public function new() super();
 
 	override public function updateShadowPosition(shadow:Mortal, oldTime:Float, newTime:Float, nextTime:Float):Void {
+		var pos:ArpPosition = shadow.position;
 		var t0:Float = oldTime - time;
 		var t1:Float = newTime - time;
-		var pos:ArpPosition = shadow.position;
-		var x:Float = this.x.accumulate(t0, t1);
-		var y:Float = this.y.accumulate(t0, t1);
-		var z:Float = this.z.accumulate(t0, t1);
-		var r:Float = this.r.accumulate(t0, t1);
-		var s:Float = this.s.accumulate(t0, t1);
+		if (absolute) {
+			var x:Float = this.x.interpolate(t1);
+			var y:Float = this.y.interpolate(t1);
+			var z:Float = this.z.interpolate(t1);
 
-		pos.x += x + r * Math.cos(pos.dir.valueRadian) + s * Math.sin(pos.dir.valueRadian);
-		pos.y += y + r * Math.sin(pos.dir.valueRadian) - s * Math.cos(pos.dir.valueRadian);
-		pos.z += z;
+			pos.x = x;
+			pos.y = y;
+			pos.z = z;
+
+		} else {
+			var x:Float = this.x.accumulate(t0, t1);
+			var y:Float = this.y.accumulate(t0, t1);
+			var z:Float = this.z.accumulate(t0, t1);
+			var r:Float = this.r.accumulate(t0, t1);
+			var s:Float = this.s.accumulate(t0, t1);
+
+			pos.x += x + r * Math.cos(pos.dir.valueRadian) + s * Math.sin(pos.dir.valueRadian);
+			pos.y += y + r * Math.sin(pos.dir.valueRadian) - s * Math.cos(pos.dir.valueRadian);
+			pos.z += z;
+		}
 	}
 
 	override public function updateMortalPosition(field:Field, mortal:Mortal, oldTime:Float, newTime:Float, nextTime:Float, dHitType:String):Void {
+		var pos:ArpPosition = mortal.position;
 		var t0:Float = oldTime - time;
 		var t1:Float = newTime - time;
-		var pos:ArpPosition = mortal.position;
-		var x:Float = this.x.accumulate(t0, t1);
-		var y:Float = this.y.accumulate(t0, t1);
-		var z:Float = this.z.accumulate(t0, t1);
-		var r:Float = this.r.accumulate(t0, t1);
-		var s:Float = this.s.accumulate(t0, t1);
+		var dPosX:Float;
+		var dPosY:Float;
+		var dPosZ:Float;
+		if (absolute) {
+			var x:Float = this.x.interpolate(t1);
+			var y:Float = this.y.interpolate(t1);
+			var z:Float = this.z.interpolate(t1);
 
-		var dPosX:Float = x + r * Math.cos(pos.dir.valueRadian) + s * Math.sin(pos.dir.valueRadian);
-		var dPosY:Float = y + r * Math.sin(pos.dir.valueRadian) - s * Math.cos(pos.dir.valueRadian);
-		var dPosZ:Float = z;
+			dPosX = x - pos.x;
+			dPosY = y - pos.y;
+			dPosZ = z - pos.z;
+		} else {
+			var x:Float = this.x.accumulate(t0, t1);
+			var y:Float = this.y.accumulate(t0, t1);
+			var z:Float = this.z.accumulate(t0, t1);
+			var r:Float = this.r.accumulate(t0, t1);
+			var s:Float = this.s.accumulate(t0, t1);
+
+			dPosX = x + r * Math.cos(pos.dir.valueRadian) + s * Math.sin(pos.dir.valueRadian);
+			dPosY = y + r * Math.sin(pos.dir.valueRadian) - s * Math.cos(pos.dir.valueRadian);
+			dPosZ = z;
+		}
 		mortal.hitFrames.clear();
 		for (hitFrame in this.hitFrames) mortal.hitFrames.add(hitFrame);
 		mortal.moveDWithHit(field, dPosX, dPosY, dPosZ, dHitType);
