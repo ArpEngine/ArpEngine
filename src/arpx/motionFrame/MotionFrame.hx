61d9ef7e50384772115a86105099d467ee8ea1f5
kaikoga <info@mail.kaikoga.net>
2020-04-27 09:35:28 +0900

    MotionFrame is frame data only

--- src/arpx/motionFrame/MotionFrame.hx
+++ src/arpx/motionFrame/MotionFrame.hx
@@ -2,11 +2,8 @@ package arpx.motionFrame;
 
 import arp.domain.IArpObject;
 import arp.ds.ISet;
-import arpx.field.Field;
 import arpx.hitFrame.HitFrame;
-import arpx.mortal.Mortal;
 import arpx.structs.ArpParams;
-import arpx.structs.ArpPosition;
 
 @:arpType("motionFrame", "motionFrame")
 class MotionFrame implements IArpObject {
@@ -16,11 +13,4 @@ class MotionFrame implements IArpObject {
 	@:arpField public var time:Float;
 
 	public function new() return;
-
-	public function updateShadowPosition(shadow:Mortal, target:ArpPosition, oldTime:Float, newTime:Float, nextTime:Float):Void {
-	}
-
-	public function updateMortalPosition(field:Field, mortal:Mortal, target:ArpPosition, oldTime:Float, newTime:Float, nextTime:Float, dHitType:String):Void {
-		mortal.stayWithHit(field, dHitType);
-	}
 }
